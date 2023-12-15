0b79bc6cff35aab81a73be3add67e2919a7cfd12
Gridenko Maks <Maksgridenko@ya.ru>
2023-12-15 18:59:43 +0300

    Initial Commit

---
+++ ColorsSwiftUI/ColorsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorsSwiftUIApp.swift
+//  ColorsSwiftUI
+//
+//  Created by Maxim Gridenko on 15.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
